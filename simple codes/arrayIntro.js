8ea26e674fe00c019fd0146f42ac59af22651d9f
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-01-23 15:33:47 -0300

    aprendendo arrays

---
+++ simple codes/arrayIntro.js
@@ -0,0 +1,10 @@
+
+
+let foods = ['hamburger', 'pizza', 'coxinha']
+
+
+foods.push('camarão')
+
+
+let ultimaComida = foods[foods.length-1]    //SEMPRE VAI IMPRIMIR O ÚLTIMO ELEMENTO MESMO
+console.log(ultimaComida);                  //QUE ADICIONE ALGUM OUTRO DEPOIS
