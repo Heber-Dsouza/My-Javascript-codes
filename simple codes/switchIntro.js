10346ff360f4341846ad79ed046124b5e6bb17fa
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-01-25 00:15:09 -0300

    updated

---
+++ simple codes/switchIntro.js
@@ -0,0 +1,33 @@
+let cores = ['amarelo', 'rosa', 'vermelho', 'azul', 'verde', 'cinza']
+
+let x = 6
+
+switch(cores[x]) {
+    case 'amarelo':
+        console.log('a cor que você selecionou foi o ' + cores[x]);
+        break
+    
+    case 'rosa':
+        console.log('a cor que você selecionou foi o ' + cores[x]);
+        break
+
+    case 'vermelho':
+        console.log('a cor que você selecionou foi o ' + cores[x]);
+        break
+    
+    case 'azul':
+        console.log('a cor que você selecionou foi o ' + cores[x]);
+        break
+
+    case 'verde':
+        console.log('a cor que você selecionou foi o ' + cores[x]);
+        break
+
+    case 'cinza':
+        console.log('a cor que você selecionou foi o ' + cores[x]);
+        break
+
+    default:
+        console.log('você não selecionou nenhuma cor');
+        break
+}
